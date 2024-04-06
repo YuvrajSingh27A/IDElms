5e4f55cc3b16e961474cd5446923eda09167a3f5
Deepakkumar055 <deepakkumr2098@gmail.com>
2024-04-06 10:05:13 +0530

    landing page, profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,16 +1,14 @@
-import React from "react";
-import styles from "@/styles/hacker.module.css";
-
+import React, { useState } from "react";
 import Image from "next/image";
-import yoyo from "../Profile/p.jpg";
 import { CiEdit, CiBookmarkCheck } from "react-icons/ci";
-import { useState } from "react";
+import styles from "@/styles/hacker.module.css"; // Ensure this path is correct for your project structure
+import yoyo from "../Profile/p.jpg"; // Ensure this path is correct for your project structure
 
 function ProfileHeader({ username, avatar }) {
   return (
     <div className="flex items-center mb-20">
       <Image src={avatar} alt="Avatar" className="w-12 h-12 rounded-full object-cover mr-5" />
-      <h1 className="text-2xl">{username}</h1>
+      <h1 className="text-2xl" style={{ color: '#f97718' }}>{username}</h1>
     </div>
   );
 }
@@ -26,6 +24,7 @@ function ProfileInfo({ email, bio, followers, following, isEditing, handleEdit }
             value={email}
             onChange={(e) => handleEdit("email", e.target.value)}
             className="w-full py-2 px-3 text-base border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+            style={{ borderColor: '#f97718' }}
           />
         ) : (
           <span>{email}</span>
@@ -38,6 +37,7 @@ function ProfileInfo({ email, bio, followers, following, isEditing, handleEdit }
             value={bio}
             onChange={(e) => handleEdit("bio", e.target.value)}
             className="w-full py-2 px-3 text-base border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+            style={{ borderColor: '#f97718' }}
           />
         ) : (
           <p>{bio}</p>
@@ -56,8 +56,8 @@ function ProfileInfo({ email, bio, followers, following, isEditing, handleEdit }
 }
 
 const ActivityFeed = ({ activities }) => (
-  <div className="activityFeed bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto">
-    <h2 className="text-xl font-semibold mb-5">My Badges</h2>
+  <div className={`${styles.activityFeed} ${styles.glassEffect} bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto`} style={{ borderColor: '#f97718' }}>
+    <h2 className="text-xl font-semibold mb-5" style={{ color: '#f97718' }}>My Badges</h2>
     <ul>
       {activities.map((activity, index) => (
         <li key={index}>{activity}</li>
@@ -67,8 +67,8 @@ const ActivityFeed = ({ activities }) => (
 );
 
 const Certificate = ({ certification }) => (
-  <div className="activityFeed bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto">
-    <h2 className="text-xl font-semibold mb-5">Certificate Details</h2>
+  <div className={`${styles.activityFeed} ${styles.glassEffect} bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto`} style={{ borderColor: '#f97718' }}>
+    <h2 className="text-xl font-semibold mb-5" style={{ color: '#f97718' }}>Certificate Details</h2>
     <ul>
       {certification.map((cert, index) => (
         <li key={index}>{cert}</li>
@@ -78,8 +78,8 @@ const Certificate = ({ certification }) => (
 );
 
 const Activity = ({ contact }) => (
-  <div className="activityFeed bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto">
-    <h2 className="text-xl font-semibold mb-5">Contact Details</h2>
+  <div className={`${styles.activityFeed} ${styles.glassEffect} bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto`} style={{ borderColor: '#f97718' }}>
+    <h2 className="text-xl font-semibold mb-5" style={{ color: '#f97718' }}>Contact Details</h2>
     <ul>
       {contact.map((detail, index) => (
         <li key={index}>{detail}</li>
@@ -89,8 +89,8 @@ const Activity = ({ contact }) => (
 );
 
 const SolvedProblem = ({ problems }) => (
-  <div className="activityFeed bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto">
-    <h2 className="text-xl font-semibold mb-5">Solved Problem</h2>
+  <div className={`${styles.activityFeed} ${styles.glassEffect} bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto`} style={{ borderColor: '#f97718' }}>
+    <h2 className="text-xl font-semibold mb-5" style={{ color: '#f97718' }}>Solved Problem</h2>
     <ul>
       {problems.map((problem, index) => (
         <li key={index}>{problem}</li>
@@ -122,11 +122,11 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className={styles.bigContainer}>
-      <div className={styles.profilePage}>
+    <div className={`${styles.bigContainer} ${styles.customBackground}`}>
+      <div className={`${styles.profilePage} ${styles.textOnCustomBg}`}>
         <ProfileHeader username={profileState.username} avatar={yoyo} />
 
-        <button className={styles.edtBtn} onClick={toggleEdit}>
+        <button className={styles.edtBtn} onClick={toggleEdit} style={{ backgroundColor: '#f97718' }}>
           {profileState.isEditing ? <CiBookmarkCheck /> : <CiEdit />}
         </button>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,74 @@
+import React, { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import Button from '../components/Buttons/button';
-import { HiUser, HiUserGroup, HiClipboardCheck } from 'react-icons/hi'; // Icons from react-icons
+import { HiUser, HiUserGroup, HiClipboardCheck } from 'react-icons/hi';
 import Link from "next/link";
 import Topbar from "@/components/Topbar/Topbar";
 
 const Home: NextPage = () => {
- return (
-  <>
-    <Topbar />
-    <div className="flex flex-col items-center justify-center h-screen bg-black text-white"> {/* Updated here for white text */}
-      <h1 className='text-5xl font-bold text-center mt-20 mb-10'>Welcome</h1> {/* Moved and adjusted margins */}
-      <div className="flex space-x-4 mb-10 gap-20 justify-center text-start">
-        {/* Individual card for each option */}
-        <div className="border-1 shadow-xl rounded-xl bg-gradient-to-br from-slate-300 to-slate-400 w-[180px] h-[250px] hover:shadow-2xl transition duration-300 ease-in-out flex flex-col items-center justify-center">
-          <a href="https://hopingminds.com/" target="_blank" rel="noopener noreferrer">
-            <Button label="Student" icon={<HiUser size={44} />} />
-          </a>
-        </div>
-        <div className="border-1 shadow-xl rounded-xl bg-gradient-to-br from-slate-300 to-slate-400 w-[180px] h-[250px] hover:shadow-2xl transition duration-300 ease-in-out flex flex-col items-center justify-center">
-          <Link href='/Categories'>
-            <Button label="Learner" icon={<HiUserGroup size={44} />} />
-          </Link>
-        </div>
-        <div className="border-1 shadow-xl rounded-xl bg-gradient-to-br from-slate-300 to-slate-400 w-[180px] h-[250px] hover:shadow-2xl transition duration-300 ease-in-out flex flex-col items-center justify-center">
-          <Button label="Online Test" icon={<HiClipboardCheck size={44} />} />
+  const welcomeText = "Namaste..." ;
+  const [letters, setLetters] = useState<{ letter: string; delay: number }[]>([]);
+
+  useEffect(() => {
+    // Generate the letter array for the "Welcome" animation
+    const letterArray = Array.from(welcomeText).map((char, index) => ({
+      letter: char,
+      delay: index * 0.1 // Adjust this value for speed
+    }));
+    setLetters(letterArray);
+  }, []);
+
+  // State to track which card is hovered for the tilt effect
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <>
+      <Topbar />
+      <div className="landingPage bg-cover bg-center min-h-screen" style={{ backgroundImage: `url('./landingPageBg.jpg')` }}>
+        <div className="flex flex-col items-center justify-center min-h-screen  text-white">
+          <h1 className='text-5xl font-bold text-center mt-10 mb-20'>
+            {letters.map((item, index) => (
+              <span key={index} style={{
+                opacity: 0,
+                animation: `appear 0.5s forwards ${item.delay}s`,
+                display: 'inline-block'
+              }}>
+                {item.letter}
+              </span>
+            ))}
+          </h1>
+          <div className="flex space-x-4 mb-10 gap-20 justify-center text-start">
+            {['Student', 'Learner', 'Online Test'].map((label, index) => (
+              <div key={index}
+                className="border border-gray-200 shadow-xl rounded-xl bg-gradient-to-br from-transparent to-transparent w-[250px] h-[250px] transition-all duration-300 ease-in-out flex flex-col items-center justify-center bg-clip-padding backdrop-filter backdrop-blur-lg bg-white bg-opacity-10"
+                onMouseEnter={() => setHoveredIndex(index)}
+                onMouseLeave={() => setHoveredIndex(null)}
+                style={{
+                  transform: hoveredIndex === index ? 'perspective(1500px) rotateY(15deg) rotateX(15deg)' : 'none',
+                  boxShadow: hoveredIndex === index ? '0 0 20px 5px orange' : '0 4px 6px rgba(0, 0, 0, 0.1)', // Glow effect on hover
+                  filter: hoveredIndex !== null && hoveredIndex !== index ? 'blur(4px)' : 'none' // Blur effect on other cards
+                }}
+              >
+                <div className="text-4xl">
+                  {label === 'Student' && <HiUser size={44} className="text-orange-500" />}
+                  {label === 'Learner' && <HiUserGroup size={44} className="text-orange-500" />}
+                  {label === 'Online Test' && <HiClipboardCheck size={44} className="text-orange-500" />}
+                </div>
+                <div className="mt-2.5 text-lg">{label}</div>
+                <Link href='/Categories'>
+                  <button
+                    className="mt-5 bg-orange-500 text-white rounded p-2"
+                  >
+                    Learn More
+                  </button>
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </>
- );
+    </>
+  );
 };
 
 export default Home;

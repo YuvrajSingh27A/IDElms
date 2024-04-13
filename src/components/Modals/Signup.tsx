a2eb728f942e12589a11d36b0274348b1408d0eb
Deepakkumar055 <deepakkumr2098@gmail.com>
2024-04-13 12:06:45 +0530

    made changes on the user profile page

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,12 +1,12 @@
-// ./src/components/Signup.tsx
-
 import React, { useEffect, useState } from "react";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth, firestore } from "@/firebase/firebase";
-import { doc, setDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 import { sendEmailVerification } from 'firebase/auth'; 
+
+
 
 type SignupProps = {};
 
@@ -14,25 +14,44 @@ const Signup: React.FC<SignupProps> = () => {
     const router = useRouter();
     const [inputs, setInputs] = useState({ email: "", displayName: "", password: "" });
     const [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth);
+                 // user stores info about the user being autheticated  and loading is a boolean variable which indicates whether the currest user is being created if is being created then true else false
+    
 
     const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
     };
 
+   
+
     const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (!inputs.email || !inputs.password || !inputs.displayName) return alert("Please fill all fields");
+        // to check if all fields are filled or not
+
+        if (!inputs.email || !inputs.password || !inputs.displayName) return alert("Please fill all fields"); 
         try {
+
+            // creating new account i.e. signup
+
             toast.loading("Creating your account", { position: "top-center", toastId: "loadingToast" });
-            const newUserCredential = await createUserWithEmailAndPassword(inputs.email, inputs.password);
-            if (!newUserCredential || !newUserCredential.user) throw new Error("Failed to create user");
+            const newUserCredential = await createUserWithEmailAndPassword(inputs.email, inputs.password);// creating user with email and password, this process can take some tyme therefore it is await.
+            if (!newUserCredential || !newUserCredential.user) throw new Error("Failed to create user"); // if newUserCredential object exist. that is a new useer object is ccreated or not.
             
+            // storing the user email and dislayname in allUsers collection in firestore
+
+            const db = getFirestore(); //create the databse instance
+            const user = newUserCredential.user; // get the current user
+            const allUsersRef = doc(db, "allUsers", user.uid); // reference to the allUsers collection in firebase firestore db
+            await setDoc(allUsersRef, { // updating and setting the user data in firestore
+                email: user.email,
+                displayName: inputs.displayName,
+            });
+
             // Send email verification
             await sendEmailVerification(auth.currentUser!);
             toast.info("Email verification link sent! Please verify your email to complete registration.", { position: "top-center" });
 
             // Redirect user to a verification page
-            router.push(`/verification?email=${inputs.email}`);
+           // router.push(/verification?email=${inputs.email});
         } catch (error: any) {
             toast.error(error.message, { position: "top-center" });
         } finally {
@@ -77,7 +96,7 @@ const Signup: React.FC<SignupProps> = () => {
                     name='password'
                     id='password'
                     className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
-                    placeholder='*******'
+                    placeholder='***'
                 />
             </div>
 
@@ -85,7 +104,7 @@ const Signup: React.FC<SignupProps> = () => {
                 type='submit'
                 className='w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'
             >
-                {loading ? "Registering..." : "Register"}
+                {loading ? "Registering..." : "Register"} 
             </button>
 
             <div className='text-sm font-medium text-gray-300'>
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,65 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import { CiEdit, CiBookmarkCheck } from "react-icons/ci";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import styles from "@/styles/hacker.module.css"; // Ensure this path is correct for your project structure
 import yoyo from "../Profile/p.jpg"; // Ensure this path is correct for your project structure
 
-function ProfileHeader({ username, avatar }) {
-  return (
-    <div className="flex items-center mb-20">
-      <Image src={avatar} alt="Avatar" className="w-12 h-12 rounded-full object-cover mr-5" />
-      <h1 className="text-2xl" style={{ color: '#f97718' }}>{username}</h1>
-    </div>
-  );
-}
+const ProfileHeader = ({ userData }) => (
+  <div className="flex items-center mb-20">
+    <Image src={yoyo} alt="Avatar" className="w-12 h-12 rounded-full object-cover mr-5" />
+    <h1 className="text-2xl" style={{ color: '#f97718' }}>{userData && userData.username}</h1>
+  </div>
+);
 
-function ProfileInfo({ email, bio, followers, following, isEditing, handleEdit }) {
-  return (
-    <div className="mb-20">
-      <div className="mb-5">
-        <label className="font-bold text-gray-500">Email: </label>
-        {isEditing ? (
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => handleEdit("email", e.target.value)}
-            className="w-full py-2 px-3 text-base border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-            style={{ borderColor: '#f97718' }}
-          />
-        ) : (
-          <span>{email}</span>
-        )}
-      </div>
-      <div className="mb-5">
-        <label className="font-bold text-gray-500">Bio: </label>
-        {isEditing ? (
-          <textarea
-            value={bio}
-            onChange={(e) => handleEdit("bio", e.target.value)}
-            className="w-full py-2 px-3 text-base border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-            style={{ borderColor: '#f97718' }}
-          />
-        ) : (
-          <p>{bio}</p>
-        )}
-      </div>
-      <div className="mb-5">
-        <label className="font-bold text-gray-500">Followers: </label>
-        <span>{followers}</span>
-      </div>
-      <div>
-        <label className="font-bold text-gray-500">Following: </label>
-        <span>{following}</span>
-      </div>
-    </div>
-  );
-}
+
 
 const ActivityFeed = ({ activities }) => (
   <div className={`${styles.activityFeed} ${styles.glassEffect} bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto`} style={{ borderColor: '#f97718' }}>
     <h2 className="text-xl font-semibold mb-5" style={{ color: '#f97718' }}>My Badges</h2>
     <ul>
-      {activities.map((activity, index) => (
+      {activities && activities.map((activity, index) => (
         <li key={index}>{activity}</li>
       ))}
     </ul>
@@ -70,7 +30,7 @@ const Certificate = ({ certification }) => (
   <div className={`${styles.activityFeed} ${styles.glassEffect} bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto`} style={{ borderColor: '#f97718' }}>
     <h2 className="text-xl font-semibold mb-5" style={{ color: '#f97718' }}>Certificate Details</h2>
     <ul>
-      {certification.map((cert, index) => (
+      {certification && certification.map((cert, index) => (
         <li key={index}>{cert}</li>
       ))}
     </ul>
@@ -81,7 +41,7 @@ const Activity = ({ contact }) => (
   <div className={`${styles.activityFeed} ${styles.glassEffect} bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto`} style={{ borderColor: '#f97718' }}>
     <h2 className="text-xl font-semibold mb-5" style={{ color: '#f97718' }}>Contact Details</h2>
     <ul>
-      {contact.map((detail, index) => (
+      {contact && contact.map((detail, index) => (
         <li key={index}>{detail}</li>
       ))}
     </ul>
@@ -92,7 +52,7 @@ const SolvedProblem = ({ problems }) => (
   <div className={`${styles.activityFeed} ${styles.glassEffect} bg-gray-800 text-white p-8 rounded-2xl shadow mb-20 w-auto`} style={{ borderColor: '#f97718' }}>
     <h2 className="text-xl font-semibold mb-5" style={{ color: '#f97718' }}>Solved Problem</h2>
     <ul>
-      {problems.map((problem, index) => (
+      {problems && problems.map((problem, index) => (
         <li key={index}>{problem}</li>
       ))}
     </ul>
@@ -100,9 +60,12 @@ const SolvedProblem = ({ problems }) => (
 );
 
 const ProfilePage = () => {
-  const [profileState, setProfileState] = useState({
-    username: "deepak kumar",
-    email: "dk0133964@gmail.com",
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    
+  });
+  const [data,setData] = useState({
     bio: "web developer",
     followers: 100,
     following: 50,
@@ -110,43 +73,73 @@ const ProfilePage = () => {
     contact: ["add Your Mail", "+91 7018318078", "india"],
     certification: ["You Have Not Any Certificates yet, Get Certified"],
     solvedProblems: ["Problem A", "Problem B", "Problem C"],
-    isEditing: false,
-  });
+  })
+  const ProfileInfo = ({ userData }) => (
+    <div className="mb-20">
+      <div className="mb-5">
+        <label className="font-bold text-gray-500">Email: </label>
+        <span>{userData && userData.email}</span>
+      </div>
+      <div className="mb-5">
+        <label className="font-bold text-gray-500">userName: </label>
+        <span>{userData && userData.displayName}</span>
+      </div>
+      <div className="mb-5">
+        <label className="font-bold text-gray-500">Bio: </label>
+        <p>{data && data.bio}</p>
+      </div>
+      <div className="mb-5">
+        <label className="font-bold text-gray-500">Followers: </label>
+        <span>{data && data.followers}</span>
+      </div>
+      <div>
+        <label className="font-bold text-gray-500">Following: </label>
+        <span>{data && data.following}</span>
+      </div>
+    </div>
+  );
+  const [user] = useAuthState(auth);
 
-  const handleEdit = (field, value) => {
-    setProfileState((prevState) => ({ ...prevState, [field]: value }));
-  };
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (user) {
+        try {
+          // Fetch user data from Firestore
+          const db = getFirestore();
+          const allUsersRef = doc(db, "allUsers", user.uid);
+          const userDoc = await getDoc(allUsersRef); 
+          if (userDoc.exists()) {
+            // If user data exists, set it in state
+            setUserData(userDoc.data());
+          } else {
+            console.log("User data not found");
+          }
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      }
+    };
 
-  const toggleEdit = () => {
-    setProfileState((prevState) => ({ ...prevState, isEditing: !prevState.isEditing }));
-  };
+    fetchUserData();
+  }, [user]);
 
   return (
     <div className={`${styles.bigContainer} ${styles.customBackground}`}>
       <div className={`${styles.profilePage} ${styles.textOnCustomBg}`}>
-        <ProfileHeader username={profileState.username} avatar={yoyo} />
-
-        <button className={styles.edtBtn} onClick={toggleEdit} style={{ backgroundColor: '#f97718' }}>
-          {profileState.isEditing ? <CiBookmarkCheck /> : <CiEdit />}
-        </button>
-
+        <ProfileHeader userData={userData} />
+        
         <ProfileInfo
-          email={profileState.email}
-          bio={profileState.bio}
-          followers={profileState.followers}
-          following={profileState.following}
-          isEditing={profileState.isEditing}
-          handleEdit={handleEdit}
+          userData={userData}
         />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-          <ActivityFeed activities={profileState.activities} />
-          <Certificate certification={profileState.certification} />
+          <ActivityFeed activities={data.activities} />
+          <Certificate certification={data.certification} />
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-          <Activity contact={profileState.contact} />
-          <SolvedProblem problems={profileState.solvedProblems} />
+          <Activity contact={data.contact} />
+          <SolvedProblem problems={data.solvedProblems} />
         </div>
       </div>
     </div>

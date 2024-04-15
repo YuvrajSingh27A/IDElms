cc12b9c39fc75ed669c9d5ec5113d870d4c3f3b7
VanshMehra19 <142845640+VanshMehra19@users.noreply.github.com>
2024-04-15 10:08:48 +0530

    made changes in landing pade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage= () => {
         {/* Left side */}
         <div className="w-1/3 p-4 flex flex-col justify-center items-center"> {/* Reduced width to 1/3 */}
           {/* Student Card */}
-          <div className="bg-white rounded-lg shadow-md p-4 mb-4  ml-56 relative aspect-w-1 aspect-h-1 transform rotate-0 transition-transform duration-500 hover:rotate-360 hover:scale-105 backdrop-filter backdrop-blur-lg bg-opacity-20 w-11/12 hover:shadow-orange">
+          <div className="bg-white rounded-lg shadow-md p-4 mb-4  ml-48 relative aspect-w-1 aspect-h-1 transform rotate-0 transition-transform duration-500 hover:rotate-360 hover:scale-105 backdrop-filter backdrop-blur-lg bg-opacity-20 w-11/12 hover:shadow-orange">
             <h2 className="text-lg font-semibold mb-2 text-white">Student</h2>
             <p className="text-sm text-white mb-4">Short description about the student card goes here.</p>
             <Link href="/StudentHome"><button className="bg-orange-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg transition-colors duration-300">
@@ -22,7 +22,7 @@ const Home: NextPage= () => {
             </button></Link>
           </div>
           {/* Trainer Card */}
-          <div className="bg-white rounded-lg ml-56 shadow-md p-4 relative aspect-w-1 aspect-h-1 transform rotate-0 transition-transform duration-500 hover:rotate-360 hover:scale-105 backdrop-filter backdrop-blur-lg bg-opacity-20 w-11/12 hover:shadow-orange">
+          <div className="bg-white rounded-lg ml-48 shadow-md p-4 relative aspect-w-1 aspect-h-1 transform rotate-0 transition-transform duration-500 hover:rotate-360 hover:scale-105 backdrop-filter backdrop-blur-lg bg-opacity-20 w-11/12 hover:shadow-orange">
             <h2 className="text-lg font-semibold mb-2 text-white">Trainer</h2>
             <p className="text-sm text-white mb-4">Short description about the trainer card goes here.</p>
             <button className="bg-orange-500 hover:bg-blue-500 text-white font-semibold px-4 py-2 rounded-lg transition-colors duration-300">
